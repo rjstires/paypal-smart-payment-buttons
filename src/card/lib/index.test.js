b2815ebfa40841ef8c6e15fb892101187dd5bd7c
Oscar Leon <63573634+oscarleonnogales@users.noreply.github.com>
2023-03-01 08:42:01 -0600

    Fixed bug where cardExpiryToPaymentSourceExpiry returns the wrong date (#562)

--- src/card/lib/index.test.js
+++ src/card/lib/index.test.js
@@ -1,5 +1,5 @@
 /* @flow */
-import { convertCardToPaymentSource } from "./index";
+import { convertCardToPaymentSource, cardExpiryToPaymentSourceExpiry } from "./index";
 
 const testCard = "4111111111111111";
 const testSecurityCode = "100";
@@ -60,5 +60,50 @@ describe("convertCardToPaymentSource", () => {
         cvv: testSecurityCode,
       })
     ).toThrowError(`can not convert invalid expiry date: ${badDate}`);
+  });
+});
+
+describe("cardExpiryToPaymentSourceExpiry", () => {
+  it("returns the same input string when the input string is already in YYYY-mm format", () => {
+    const input = "2023-02";
+    expect(cardExpiryToPaymentSourceExpiry(input)).toEqual(input);
+  });
+
+  it("converts mm/YYYY input to YYYY-mm format for first month", () => {
+    const input1 = "01/2024";
+    const input2 = "1/24";
+    const expectedOutput = "2024-01";
+    expect(cardExpiryToPaymentSourceExpiry(input1)).toEqual(expectedOutput);
+    expect(cardExpiryToPaymentSourceExpiry(input2)).toEqual(expectedOutput);
+  });
+
+  it("converts mm/YYYY input to YYYY-mm format for a middle month", () => {
+    const input1 = "04/2024";
+    const input2 = "4/24";
+    const expectedOutput = "2024-04";
+    expect(cardExpiryToPaymentSourceExpiry(input1)).toEqual(expectedOutput);
+    expect(cardExpiryToPaymentSourceExpiry(input2)).toEqual(expectedOutput);
+  });
+
+  it("converts mm/YYYY input to YYYY-mm format for last month", () => {
+    const input1 = "12/2024";
+    const input2 = "12/24";
+    const expectedOutput = "2024-12";
+    expect(cardExpiryToPaymentSourceExpiry(input1)).toEqual(expectedOutput);
+    expect(cardExpiryToPaymentSourceExpiry(input2)).toEqual(expectedOutput);
+  });
+
+  it("throws an error when the input string is not in the expected format", () => {
+    const input = "2023/02";
+    expect(() => {
+      cardExpiryToPaymentSourceExpiry(input);
+    }).toThrowError(`can not convert invalid expiry date: ${input}`);
+  });
+
+  it("throws an error when the input is empty", () => {
+    const input = "";
+    expect(() => {
+      cardExpiryToPaymentSourceExpiry(input);
+    }).toThrowError(`can not convert invalid expiry date: ${input}`);
   });
 });
