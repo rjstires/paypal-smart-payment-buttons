1a2cda9b37458f7d8abe3bd235184482b395927c
Oscar Leon <oscarleonnogales@gmail.com>
2023-03-06 13:40:36 -0600

    chore: deleted unused utility function

--- src/card/interface/reformatExpiry.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-// Reformat MM/YYYY to YYYY-MM
-export function reformatExpiry(expiry: ?string): ?string {
-  if (typeof expiry === "string") {
-    const [month, year] = expiry.split("/");
-    return `${year}-${month}`;
-  }
-}
