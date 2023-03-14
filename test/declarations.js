ce42fd9513568dd1cd8534ebbfde20be73e8cc98
Dean Wood <bywood@paypal.com>
2023-03-14 16:42:44 -0700

    Added afterEach declaration to fix Flow errors in tests.

--- test/declarations.js
+++ test/declarations.js
@@ -2,6 +2,7 @@
 
 declare var afterAll : Function;
 declare var beforeEach : Function;
+declare var afterEach : Function;
 declare var it : Function;
 declare var describe : Function;
 declare var test : Function;
